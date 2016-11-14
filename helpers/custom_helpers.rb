329b1e3833d31e1f1530afca539f062b094123ab
Claudio Vallejo <CVP@claudio.local>
2016-11-14 17:54:22 -0600

    Created custom_helpers.rb for pretty_number and pretty_hour functions.

---
+++ helpers/custom_helpers.rb
@@ -0,0 +1,27 @@
+module CustomHelpers
+  # Pretty Number: Transforms '(81) 8363 2324' -> '8183632324'
+  def pretty_number telephone
+    number = telephone.gsub(' ', '').gsub('(', '').gsub(')', '')
+  end
+  # Pretty Hour: Transforms '8' -> '8:00am' and '18.25' a '6:15pm'
+  def pretty_hour number
+    time = "Cómprate un reloj!"
+    hour = number.floor
+    minutes = number - hour
+    time_zone = "am"
+    if hour > 12
+      hour -= 12
+      time_zone = "pm"
+    end
+    time = "#{hour}:"
+    minutes *= 60
+    if (minutes.floor) == 0
+      time += "00"
+    elsif 0 < minutes && minutes < 10
+      time += "0#{minutes}"
+    else
+      time += "#{minutes.floor}"
+    end
+    time += "#{time_zone}"
+  end
+end
